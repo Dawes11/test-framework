fbd4f328c5c54f3ab2b6bce56486f49c3e46d58c
Dawes11 <stipskyd@seznam.cz>
2022-06-12 19:21:30 +0200

    Challenging DOM scenario done

--- E2EGiacomTestAutomation/Pages/DomPage/DomPage.Executors.cs
+++ E2EGiacomTestAutomation/Pages/DomPage/DomPage.Executors.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace E2EGiacomTestAutomation.Pages.DomPage
 {
     using Pages.CommonPage;
+    using Utilities.Extensions;
 
     public partial class DomPage : CommonPage
     {
+        public int FindColumn(string tableHeaderName)
+        {
+            return TableHeaderElements.FindIndex(elm => elm.Text.Equals(tableHeaderName));
+        }
+
+        public string GetValueFromTable(string column, string row)
+        {
+            return TableValue(column, row);
+        }
+
     }
 }
--- E2EGiacomTestAutomation/Tests/Steps/DomPageSteps.cs
+++ E2EGiacomTestAutomation/Tests/Steps/DomPageSteps.cs
@@ -9,6 +9,14 @@ using E2EGiacomTestAutomation.Pages.DomPage;
 
 namespace E2EGiacomTestAutomation.Tests.Steps
 {
+    using FluentAssertions;
+    using Pages.LoginPage;
+    using E2EGiacomTestAutomation.Utilities.Extensions;
+    using TechTalk.SpecFlow;
+    using Utilities;
+    using Utilities.Helpers.TestDataGenerator;
+    using Utilities.Enums;
+
     [Binding]
     public class DomPageSteps
     {
@@ -17,7 +25,19 @@ namespace E2EGiacomTestAutomation.Tests.Steps
         [Then(@"I can see correct values in rows")]
         public void CorrectValuesAreInRows(Table table)
         {
-            ScenarioContext.Current.Pending();
+            foreach (var row in table.Rows)
+            {
+                var column = domPage.GetColumnIndexByName(row[0]);
+
+                //When column is not found, -1 value is assigned
+                column.Should().BeGreaterThan(-1, $"Column with name {row[0]} was not found.");
+                
+                //Column value counts from zero, column value in table from 1 -> +1 added
+                column++;
+
+                var value = domPage.GetValueFromTable(column.ToString(), row[1]);
+                value.Should().BeEquivalentTo(row[2], $"Expected value on coordinates row: {row[1]}, column: {column.ToString()} was: {row[2]}, but found {value}");
+            }
         }
 
     }
